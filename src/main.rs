54ecb1edf1df1822b22552f5573f329246b7f7b1
bcorey <benjaminmcorey@gmail.com>
2025-12-20 13:44:53 -0500

    miniamL

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,13 @@
 fn main() {
-    // Some simple CLI args requirements...
-    let url = match std::env::args().nth(1) {
-        Some(url) => url,
-        None => {
-            println!("No CLI URL provided, using default.");
-            "https://hyper.rs".into()
-        }
-    };
+    let url = std::env::args().nth(1).unwrap_or("https://hyper.rs".into());
 
-    eprintln!("Fetching {url:?}...");
+    println!("Fetching {url}...");
+    let mut res = reqwest::blocking::get(url).expect("Request failed.");
 
-    // reqwest::blocking::get() is a convenience function.
-    //
-    // In most cases, you should create/build a reqwest::Client and reuse
-    // it for all requests.
-    let mut res = reqwest::blocking::get(url).unwrap();
-
-    eprintln!("Response: {:?} {}", res.version(), res.status());
-    eprintln!("Headers: {:#?}\n", res.headers());
+    println!("Response: {:?} {}", res.version(), res.status());
+    println!("Headers: {:#?}\n", res.headers());
 
     // copy the response body directly to stdout
-    res.copy_to(&mut std::io::stdout()).unwrap();
+    res.copy_to(&mut std::io::stdout())
+        .expect("Failed to print response body");
 }
