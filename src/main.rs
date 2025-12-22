bca7f6eb8e52b1ddbca02bcf3acad6ef926cf401
bcorey <benjaminmcorey@gmail.com>
2025-12-21 20:02:36 -0500

    convert args into Input

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,51 @@
+use std::env::Args;
+
 fn main() {
-    let url = std::env::args().nth(1).unwrap_or("https://hyper.rs".into());
+    let input: Input = std::env::args().into();
+    println!("Fetching {}...", input.url);
+    let client = reqwest::blocking::Client::new();
+    let request = match input.request_type {
+        RequestType::Get => client.get(input.url),
+        RequestType::Post => client.post(input.url),
+    };
+    let mut response = request.send().expect("Failed to send request");
 
-    println!("Fetching {url}...");
-    let mut res = reqwest::blocking::get(url).expect("Request failed.");
-
-    println!("Response: {:?} {}", res.version(), res.status());
-    println!("Headers: {:#?}\n", res.headers());
+    println!("Response: {:?} {}", response.version(), response.status());
+    println!("Headers: {:#?}\n", response.headers());
 
     // copy the response body directly to stdout
-    res.copy_to(&mut std::io::stdout())
+    response
+        .copy_to(&mut std::io::stdout())
         .expect("Failed to print response body");
+}
+
+struct Input {
+    request_type: RequestType,
+    url: String,
+}
+
+impl From<Args> for Input {
+    fn from(mut args: Args) -> Self {
+        let request_type = args.next().map_or(RequestType::default(), |arg| arg.into());
+        let url = args.next().unwrap_or("https://hyper.rs".into());
+        println!("{request_type:?} {url}");
+        Self { request_type, url }
+    }
+}
+
+#[derive(Default, Debug)]
+enum RequestType {
+    #[default]
+    Get,
+    Post,
+}
+
+impl From<String> for RequestType {
+    fn from(value: String) -> Self {
+        match value.to_lowercase().as_str() {
+            "get" => Self::Get,
+            "post" => Self::Post,
+            _ => Self::Get,
+        }
+    }
 }
